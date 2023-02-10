9852f8742ec6fb863b71d22cb773bd3ac21ba9d3
Jocelyn <jjayjack@dminc.com>
2023-02-10 17:51:32 -0600

    Initial routing; custom header

--- index.js
+++ index.js
@@ -5,7 +5,18 @@ const url = require('url');
 // const files = require('./files');
 
 const server = http.createServer((req, res) => {
-	res.end('Hello from the server');
+	const pathName = req.url;
+	if (pathName === '/' || pathName === '/overview') {
+		res.end('This is the overview');
+	} else if (pathName === '/product') {
+		res.end('This is the product');
+	} else {
+		res.writeHead(404, {
+			'Content-type': 'text/html',
+			'Custom-header-my-own': 'hello-world'
+		});
+		res.end('<h1>Page not found</h1>');
+	}
 });
 
 server.listen(8080, '127.0.0.1', () => {
