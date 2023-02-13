d4c330f21ba89cbb301be8b92650d81a6c69978f
Jocelyn <jjayjack@dminc.com>
2023-02-13 13:37:13 -0600

    Basic local api call

--- index.js
+++ index.js
@@ -4,16 +4,24 @@ const url = require('url');
 
 // const files = require('./files');
 
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObject = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
 	const pathName = req.url;
 	if (pathName === '/' || pathName === '/overview') {
 		res.end('This is the overview');
 	} else if (pathName === '/product') {
 		res.end('This is the product');
+	} else if (pathName === '/api') {
+		res.writeHead(200, {
+			'Content-type': 'application/json'
+		});
+		res.end(data);
 	} else {
 		res.writeHead(404, {
 			'Content-type': 'text/html',
-			'Custom-header-my-own': 'hello-world'
+			'Custom-header-my-own': 'requested-address-not-available'
 		});
 		res.end('<h1>Page not found</h1>');
 	}
