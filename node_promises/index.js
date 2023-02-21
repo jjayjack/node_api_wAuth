5afd2d5e6e5d1b3a7ab87f9b2e649db0eac7be45
Jocelyn <jjayjack@dminc.com>
2023-02-21 11:18:46 -0600

    Setting up Promise

--- node_promises/index.js
+++ node_promises/index.js
@@ -1,6 +1,34 @@
 const fs = require('fs');
 const superagent = require('superagent');
 
+const readFilePro = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject('I could not find that...');
+      resolve(data);
+    });
+  });
+};
+
+readFilePro(`${__dirname}/dog.txt`).then((data) => {
+  console.log(`Breed: ${data}`);
+
+  return superagent
+    .get(`https://dog.ceo/api/breed/${data}/images/random`)
+    .then((res) => {
+      console.log(res.body.message);
+
+      return fs.writeFile('dog-img.txt', res.body.message, (err) => {
+        if (err) return console.log(err.message);
+        console.log('Random dog image saved to file!');
+      });
+    })
+    .catch((err) => {
+      console.log('line 32', err.message);
+    });
+});
+
+/*
 fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
   console.log(`Breed: ${data}`);
 
@@ -24,3 +52,4 @@ fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
       console.log(err.message);
     });
 });
+*/
