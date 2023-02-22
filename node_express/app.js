5931bd16a99a85830a9b9b66450a6953716d069a
Jocelyn <jjayjack@dminc.com>
2023-02-22 12:11:03 -0600

    POST: set up route

--- node_express/app.js
+++ node_express/app.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.status(200).json({ message: 'Hello!', app: 'Natours' });
 });
 
+app.post('/', (req, res) => {
+  res.json({ message: 'you can post to this endpoint' });
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`App on ${port}`);
