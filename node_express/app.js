043d2ab134f29ce23a1e3463dc65eeb3332a8852
Jocelyn <jjayjack@dminc.com>
2023-02-23 16:47:40 -0600

    Start of re-factoring

--- node_express/app.js
+++ node_express/app.js
@@ -5,17 +5,8 @@ const app = express();
 // Middleware
 app.use(express.json());
 
-/*
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello!', app: 'Natours' });
-});
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data.data/tours-simple.json`));
 
-app.post('/', (req, res) => {
-  res.json({ message: 'you can post to this endpoint' });
-});
-*/
-
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 // Routing
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
