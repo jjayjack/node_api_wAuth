0b2273075b037f34d390ff4f33668a3c7ee48994
Jocelyn <jjayjack@dminc.com>
2023-02-24 12:29:55 -0600

    Comments cleaned up

--- node_express/app.js
+++ node_express/app.js
@@ -128,13 +128,8 @@ const deleteUser = (req, res) => {
 
 // Routes
 app.route('/api/v1/tours').get(getAllTours).post(createTour);
-// app.get('/api/v1/tours', getAllTours);
-// app.post('/api/v1/tours', createTour);
 
 app.route('/api/v1/tours/:id').get(getTour).patch(updateTour).delete(deleteTour);
-// app.get('/api/v1/tours/:id', getTour);
-// app.patch('/api/v1/tours/:id', updateTour);
-// app.delete('/api/v1/tours/:id', deleteTour);
 
 app.route('/api/v1/users').get(getAllUsers).post(createUser);
 
