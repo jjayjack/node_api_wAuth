7cf6ffd3100a7a57f15472bab3cf87306225f068
Jocelyn <jjayjack@dminc.com>
2023-02-24 14:39:32 -0600

    Tour controllers moved to own file

--- node_express/routes/tourRoutes.js
+++ node_express/routes/tourRoutes.js
@@ -1,79 +1,10 @@
 const express = require('express');
-const fs = require('fs');
+//const { getAllTours, getTour, createTour, updateTour, deleteTour } = require('../controllers/tourController');
+const tourController = require('./../controllers/tourController');
 
 const router = express.Router();
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`));
-
-const getAllTours = (req, res) => {
-  console.log(req.requestTime);
-  res.status(200).json({
-    status: 'success',
-    requestedTime: req.requestTime,
-    results: tours.length,
-    date: {
-      tours,
-    },
-  });
-};
-
-const getTour = (req, res) => {
-  const id = req.params.id * 1;
-
-  const tour = tours.find((el) => el.id === id);
-
-  if (!tour) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-};
-
-const createTour = (req, res) => {
-  const newId = tours[tours.legnth - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-
-  tours.push(newTour);
-
-  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), (err) => {
-    console.log(err);
-    res.status(201).json({
-      status: 'success',
-      data: {
-        tour: newTour,
-      },
-    });
-  });
-};
-
-const updateTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-  res.status(200).json({
-    status: 'success',
-    data: { tour: '<Updated tour here..>' },
-  });
-};
-
-const deleteTour = (req, res) => {
-  if (!req.params.id) {
-    return res.status(404).json({
-      status: 'fail',
-      messsage: 'Invalid',
-    });
-  }
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
-};
-
-router.route('/').get(getAllTours).post(createTour);
-router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
+router.route('/').get(tourController.getAllTours).post(tourController.createTour);
+router.route('/:id').get(tourController.getTour).patch(tourController.updateTour).delete(tourController.deleteTour);
 
 module.exports = router;
