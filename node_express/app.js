a8e2e942e8d63e6f2cfc44c8ff33a4b3a8d7aa48
Jocelyn <jjayjack@dminc.com>
2023-02-24 13:54:03 -0600

    Separating routes into own files/folder

--- node_express/app.js
+++ node_express/app.js
@@ -21,73 +21,6 @@ app.use((req, res, next) => {
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 // Tours HTTP methods
-const getAllTours = (req, res) => {
-  console.log(req.requestTime);
-  res.status(200).json({
-    status: 'success',
-    requestedTime: req.requestTime,
-    results: tours.length,
-    date: {
-      tours,
-    },
-  });
-};
-
-const getTour = (req, res) => {
-  const id = req.params.id * 1;
-
-  const tour = tours.find((el) => el.id === id);
-
-  if (!tour) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-};
-
-const createTour = (req, res) => {
-  const newId = tours[tours.legnth - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-
-  tours.push(newTour);
-
-  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), (err) => {
-    console.log(err);
-    res.status(201).json({
-      status: 'success',
-      data: {
-        tour: newTour,
-      },
-    });
-  });
-};
-
-const updateTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-  res.status(200).json({
-    status: 'success',
-    data: { tour: '<Updated tour here..>' },
-  });
-};
-
-const deleteTour = (req, res) => {
-  if (!req.params.id) {
-    return res.status(404).json({
-      status: 'fail',
-      messsage: 'Invalid',
-    });
-  }
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
-};
 
 //User HTTP methods
 const getAllUsers = (req, res) => {
@@ -125,7 +58,6 @@ const deleteUser = (req, res) => {
   });
 };
 
-const tourRouter = express.Router();
 const userRouter = express.Router();
 
 // Mounting new Router on a Route
@@ -133,8 +65,6 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 //update to Routes using Middleware
-tourRouter.route('/').get(getAllTours).post(createTour);
-tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
 userRouter.route('/').get(getAllUsers).post(createUser);
 userRouter.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
