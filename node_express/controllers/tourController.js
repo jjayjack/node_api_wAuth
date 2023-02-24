f36c0f9cbb3e5d925db3bcfbaf56613c8ff65f02
Jocelyn <jjayjack@dminc.com>
2023-02-24 16:15:59 -0600

    Custom Middleware: data validation for POST

--- node_express/controllers/tourController.js
+++ node_express/controllers/tourController.js
@@ -13,6 +13,17 @@ exports.checkID = (req, res, next, val) => {
   next();
 };
 
+exports.checkBody = (req, res, next) => {
+  console.log(`Name & price found!`);
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Invalid request - missing name or price',
+    });
+  }
+  next();
+};
+
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
@@ -31,12 +42,12 @@ exports.getTour = (req, res) => {
 };
 
 exports.createTour = (req, res) => {
-  const newId = tours[tours.legnth - 1].id + 1;
+  const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
 
   tours.push(newTour);
 
-  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), (err) => {
+  fs.writeFile(`${__dirname}/../dev-data/data/tours-simple.json`, JSON.stringify(tours), (err) => {
     console.log(err);
     res.status(201).json({
       status: 'success',
