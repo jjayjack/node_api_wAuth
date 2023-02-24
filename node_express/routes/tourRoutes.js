9c4e2876c700ca6405e0172f990113255c56d481
Jocelyn <jjayjack@dminc.com>
2023-02-24 15:32:23 -0600

    Update to use Middleware

--- node_express/routes/tourRoutes.js
+++ node_express/routes/tourRoutes.js
@@ -3,10 +3,7 @@ const express = require('express');
 const tourController = require('./../controllers/tourController');
 
 const router = express.Router();
-router.param('id', (req, res, next, val) => {
-  console.log(`Tour id is ${val}`);
-  next();
-});
+router.param('id', tourController.checkID);
 
 router.route('/').get(tourController.getAllTours).post(tourController.createTour);
 router.route('/:id').get(tourController.getTour).patch(tourController.updateTour).delete(tourController.deleteTour);
