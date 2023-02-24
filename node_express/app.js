4260505bed219cd94f37fdf9807a1859eabe78e1
Jocelyn <jjayjack@dminc.com>
2023-02-24 09:15:15 -0600

    PATCH method update

--- node_express/app.js
+++ node_express/app.js
@@ -87,23 +87,38 @@ const createTour = (req, res) => {
 
 app.post('/api/v1/tours', createTour);
 
-app.post('/api/v1/tours', (req, res) => {
-  console.log(req.body);
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
+// app.post('/api/v1/tours', (req, res) => {
+//   console.log(req.body);
+//   const newId = tours[tours.length - 1].id + 1;
+//   const newTour = Object.assign({ id: newId }, req.body);
 
-  tours.push(newTour);
+//   tours.push(newTour);
 
-  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), (err) => {
-    console.log(err);
-    res.status(201).json({
-      status: 'success',
-      data: {
-        tour: newTour,
-      },
+//   fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), (err) => {
+//     console.log(err);
+//     res.status(201).json({
+//       status: 'success',
+//       data: {
+//         tour: newTour,
+//       },
+//     });
+//   });
+// });
+
+const updateTour = (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
     });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: { tour: '<Updated tour here..>' },
   });
-});
+};
+
+app.patch('/api/v1/tours/:id', updateTour);
 
 app.patch('/api/v1/tours/:id', (req, res) => {
   if (req.params.id * 1 > tours.length) {
